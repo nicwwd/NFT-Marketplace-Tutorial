c76a7c2ef3deed34d4b00a564a0a53838e2685b6
Nic Wong <876942@qq.com>
2022-08-19 06:50:41 +0000

    1

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -180,3 +180,4 @@ contract NFTMarketplace is ERC721URIStorage {
     //In that case, tokens won't be listed by default but users can send a request to actually list a token
     //Currently NFTs are listed by default
 }
+
